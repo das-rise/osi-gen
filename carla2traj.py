28ce4a73f339def696935bf1616deadfce7c58d8
Bastian Havers-Zulka <bastian.havers-zulka@ri.se>
2025-11-04 13:03:40 +0100

    steer towards pure internal representation of trajectory, with optional metadata

--- carla2traj.py
+++ carla2traj.py
@@ -2,27 +2,7 @@ import carla
 from typing import Tuple
 
 
-class Carla2OSI:
-
-    def __init__(
-        self,
-        map_reference: str,
-        country_code: int,
-        version_major: int,
-        version_minor: int,
-        version_patch: int,
-        proj_frame_offset_position: tuple,
-        proj_frame_offset_yaw: float,
-        proj_string: str,
-    ):
-        self.map_reference = map_reference
-        self.country_code = country_code
-        self.version_major = version_major
-        self.version_minor = version_minor
-        self.version_patch = version_patch
-        self.proj_frame_offset_position = proj_frame_offset_position
-        self.proj_frame_offset_yaw = proj_frame_offset_yaw
-        self.proj_string = proj_string
+class Carla2Traj:
 
     def process_world_snapshot(self, world_snapshot: carla.WorldSnapshot):
 
@@ -59,20 +39,44 @@ class Carla2OSI:
             movingobject_vehicleclassification_type = None  # Other, car, delivery van, semitrailer, trailer, motorbike, bicycle, bus, tram, train, wheelchair, standup scooter
             movingobject_vehicleclassification_role = None  # Other, civil, ambulance, fire, police, public transport, road assistance, garbage collection, road construction, military
 
-    def _convert_geometry_to_osi_x(self, x: float) -> float:
+    def _convert_coord_to_osi_x(self, x: float) -> float:
         return 0
     
-    def _convert_geometry_to_osi_y(self, y: float) -> float:
+    def _convert_coord_to_osi_y(self, y: float) -> float:
         return 0
     
-    def _convert_geometry_to_osi_z(self, z: float) -> float:
+    def _convert_coord_to_osi_z(self, z: float) -> float:
         return 0
     
-    def _convert_geometry_to_osi_carlaVector3D(self, vector: carla.Vector3D) -> Tuple[float, float, float]:
-        x_osi = self._convert_geometry_to_osi_x(vector.x)
-        y_osi = self._convert_geometry_to_osi_y(vector.y)
-        z_osi = self._convert_geometry_to_osi_z(vector.z)
+    def _convert_carlaVector3D_to_osi(self, vector: carla.Vector3D) -> Tuple[float, float, float]:
+        x_osi = self._convert_coord_to_osi_x(vector.x)
+        y_osi = self._convert_coord_to_osi_y(vector.y)
+        z_osi = self._convert_coord_to_osi_z(vector.z)
         return (x_osi, y_osi, z_osi)
+
+    def add_map_reference(self, map_reference: str):
+        self.map_reference = map_reference
+
+    def add_country_code(self, country_code: int):
+        self.country_code = country_code
+
+    def add_version_major(self, version_major: int):
+        self.version_major = version_major
+
+    def add_version_minor(self, version_minor: int):
+        self.version_minor = version_minor
+
+    def add_version_patch(self, version_patch: int):
+        self.version_patch = version_patch
+
+    def add_proj_frame_offset_position(self, proj_frame_offset_position: tuple):
+        self.proj_frame_offset_position = proj_frame_offset_position
+
+    def add_proj_frame_offset_yaw(self, proj_frame_offset_yaw: float):
+        self.proj_frame_offset_yaw = proj_frame_offset_yaw
+
+    def add_proj_string(self, proj_string: str):
+        self.proj_string = proj_string
 
 
     # TODO: add traffic lights
