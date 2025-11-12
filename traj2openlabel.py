f8618a9d2924d85eb63245dcb70e10c8a5fe4d35
Bastian Havers-Zulka <bastian.havers-zulka@ri.se>
2025-11-12 10:16:38 +0100

    split converters into separate files

---
+++ traj2openlabel.py
@@ -0,0 +1,10 @@
+from traj2x import Traj2X
+from traj import TrajDF
+
+class Traj2OpenLabel(Traj2X):
+    """Converter to OpenLabel format."""
+
+    def __call__(self, output_path: str):
+        # Implement conversion to OpenLabel format
+        pass
+
