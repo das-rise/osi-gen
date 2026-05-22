0b66816e1531a9dee1028cd88f584c34528685c4
Bastian Havers-Zulka <bastian.havers-zulka@ri.se>
2026-05-22 17:25:46 +0200

    add ego field to dataframe

--- src/traj_convert/traj.py
+++ src/traj_convert/traj.py
@@ -28,6 +28,7 @@ class TrajDF(pl.DataFrame):
                 "type": pl.Utf8,
                 "vehicleclassification_type": pl.Utf8,
                 "vehicleclassification_role": pl.Utf8,
+                "ego_id": pl.Int32,
             }
         )
 
